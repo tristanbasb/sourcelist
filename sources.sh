b0b0119bce95f1afcd2c02b797396daa36d7d54d
tristanbasb <tristan.basbeaudou87@gmail.com>
2024-01-20 23:34:56 +0100

    mise à jour

--- sources.sh
+++ sources.sh
@@ -1,59 +1,23 @@
 #!/bin/bash
 
+
+if [ -f "/etc/apt/sources.list" ]; 
+then
+    cp /etc/apt/sources.list /etc/apt/sources.list.old
+fi
+
 echo "
-Choisir une option :
+#sources.list pour Debian 11
 
-1/ Debian 10
+# Debian Bullseye, dépôt principal + paquets non libres
+deb http://deb.debian.org/debian/ bullseye main contrib non-free
+deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
 
-2/ Debian 11
-"
+# Debian Bullseye, mises à jour de sécurité + paquets non libres
+deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
 
-read version
-
-if [ "$version" == "1" ] || [ "$version" == "2" ]
-then
-    if [ -f "/etc/apt/sources.list" ]; 
-    then
-        rm -r /etc/apt/sources.list
-    fi
-else 
-    echo "Cette option n'existe pas !!!"
-fi
-
-if [ "$version" == "1" ]
-then
-    echo "
-    #sources.list pour Debian 10
-
-    # Debian Buster, dépôt principal + paquets non libres
-    deb http://deb.debian.org/debian/ buster main contrib non-free
-    deb-src http://deb.debian.org/debian/ buster main contrib non-free
-
-    # Debian Buster, mises à jour de sécurité + paquets non libres
-    deb http://deb.debian.org/debian-security/ buster/updates main contrib non-free
-    deb-src http://deb.debian.org/debian-security/ buster/updates main contrib non-free
-
-    # Debian Buster, mises à jour "volatiles" + paquets non libres
-    deb http://deb.debian.org/debian/ buster-updates main contrib non-free
-    deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
-    " >> /etc/apt/sources.list
-fi
-
-if [ "$version" == "2" ]
-then
-    echo "
-    #sources.list pour Debian 11
-
-    # Debian Bullseye, dépôt principal + paquets non libres
-    deb http://deb.debian.org/debian/ bullseye main contrib non-free
-    deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
-
-    # Debian Bullseye, mises à jour de sécurité + paquets non libres
-    deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
-    deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
-
-    # Debian Bullseye, mises à jour "volatiles" + paquets non libres
-    deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
-    deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free
-    " >> /etc/apt/sources.list
-fi
+# Debian Bullseye, mises à jour "volatiles" + paquets non libres
+deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+" >> /etc/apt/sources.list
