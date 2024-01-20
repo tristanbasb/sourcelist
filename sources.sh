e22555e30da7e883169e01cb5b1af96e4d891d89
tristanbasb <tristan.basbeaudou87@gmail.com>
2024-01-20 23:35:39 +0100

    mauvaise version de debian

--- sources.sh
+++ sources.sh
@@ -7,17 +7,17 @@ then
 fi
 
 echo "
-#sources.list pour Debian 11
+#sources.list pour Debian 10
 
-# Debian Bullseye, dépôt principal + paquets non libres
-deb http://deb.debian.org/debian/ bullseye main contrib non-free
-deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
+# Debian Buster, dépôt principal + paquets non libres
+deb http://deb.debian.org/debian/ buster main contrib non-free
+deb-src http://deb.debian.org/debian/ buster main contrib non-free
 
-# Debian Bullseye, mises à jour de sécurité + paquets non libres
-deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
-deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+# Debian Buster, mises à jour de sécurité + paquets non libres
+deb http://deb.debian.org/debian-security/ buster/updates main contrib non-free
+deb-src http://deb.debian.org/debian-security/ buster/updates main contrib non-free
 
-# Debian Bullseye, mises à jour "volatiles" + paquets non libres
-deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+# Debian Buster, mises à jour "volatiles" + paquets non libres
+deb http://deb.debian.org/debian/ buster-updates main contrib non-free
+deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
 " >> /etc/apt/sources.list
